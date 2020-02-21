15902256f2d64241d42656a885104f27131b1291
rafi007akhtar <alimdrafi@gmail.com>
2020-02-21 10:17:18 +0000

    Two new featutes added: 1. Dumping is happening: text extracted from images dumped into a text file 2. Program now works with multiple images

--- app3.py
+++ app3.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 
 from openvino.inference_engine import IECore, IENetwork
+import json
 
 DETECTOR_PATH = "models/intel/text-spotting-0001-detector/FP32/text-spotting-0001-detector.xml"
 ENCODER_PATH = "models/intel/text-spotting-0001-recognizer-encoder/FP32/text-spotting-0001-recognizer-encoder.xml"
@@ -24,7 +25,9 @@ def get_args():
     c_desc = "CPU extension file location, if applicable"
     d_desc = "Device, if not CPU (GPU, FPGA, MYRIAD)"
     i_desc = "The location of the input image"
-    m_desc = "The location of the model XML file"
+    detp_desc = "The location of the Text Detector model XML file"
+    encp_desc = "The location of the Text Encoder model XML file"
+    decp_desc = "The location of the Text Decoder model XML file"
     pt_desc = "The probability threshold"
     letters_desc = "Set of letters used for decoding"
 
@@ -34,12 +37,18 @@ def get_args():
     optional = parser.add_argument_group('optional arguments')
 
     # -- Create the arguments
-    required.add_argument("-i", help=i_desc, default=INPUT_STREAM)
-    required.add_argument("-m", help=m_desc, default=DETECTOR_PATH)
+    optional.add_argument("-i", help=i_desc, default=INPUT_STREAM)
+    optional.add_argument("-detp", help=detp_desc, default=DETECTOR_PATH)
+    optional.add_argument("-encp", help=encp_desc, default=ENCODER_PATH)
+    optional.add_argument("-decp", help=decp_desc, default=DECODER_PATH)
     optional.add_argument("-c", help=c_desc, default=None)
     optional.add_argument("-d", help=d_desc, default="CPU")
     optional.add_argument("-pt", help=pt_desc, default=0.5)
-    optional.add_argument("-letters", help=letters_desc, default="  0123456789abcdefghijklmnopqrstuvwxyz")
+    optional.add_argument(
+        "-letters", 
+        help=letters_desc, 
+        default="  0123456789abcdefghijklmnopqrstuvwxyz"
+    )
     
     args = parser.parse_args()
 
@@ -48,20 +57,20 @@ def get_args():
 
 def get_bin(xml):
     return os.path.splitext(xml)[0] + ".bin"
-    
+
 def perform_inference(args):
     '''
     Performs inference on an input image, given a model.
     '''
     
     # Get all the XML and BIN paths for all the models
-    detector_xml = DETECTOR_PATH
+    detector_xml = args.detp
     detector_bin = get_bin(detector_xml)
     
-    encoder_xml = ENCODER_PATH
+    encoder_xml = args.encp
     encoder_bin = get_bin(encoder_xml)
     
-    decoder_xml = DECODER_PATH
+    decoder_xml = args.decp
     decoder_bin = get_bin(decoder_xml)
 
     # Get network instances for all models
@@ -75,7 +84,7 @@ def perform_inference(args):
     encoder_exec = ie.load_network(network=encoder_net, device_name=args.d)
     decoder_exec = ie.load_network(network=decoder_net, device_name=args.d)
     
-    # Obtain all tge shapes needed
+    # Obtain all the shapes needed
     hidden_shape = decoder_net.inputs["prev_hidden"].shape
     n, c, h, w = detector_net.inputs["im_data"].shape
     
@@ -83,78 +92,99 @@ def perform_inference(args):
     del detector_net
     del encoder_net
     del decoder_net
-
-    # Read the input imagetexts
-    image = cv2.imread(args.i)
-    scale_x = w / image.shape[1]
-    scale_y = h / image.shape[0]
-    image = cv2.resize(image, (w, h))
     
-    # Apply preprocessing on the image
-    image_size = image.shape[:2]
-    image = np.pad(
-        image, (
-        (0, h - image_size[0]), 
-        (0, w - image_size[1]),
-        (0, 0)
-        ),
-    mode='constant', constant_values=0
-    )
-    image = image.transpose((2, 0, 1))
-    image = image.reshape((n, c, h, w)).astype(np.float32)
+    # Create a new dump file for texts
+    f = open("dump.txt", "w")
+    f.close()
     
-    # Get the info for inference
-    info = np.asarray([[image_size[0], image_size[1], 1]], dtype=np.float32)
-
-    # Perform first inference - for text spotting - and parse the output
-    output = detector_exec.infer({"im_data": image, "im_info": info})
-
-    boxes = output["boxes"]
-    scores = output["scores"]
-    classes = output["classes"].astype(np.uint32)
-    rms = output["raw_masks"]
-    tfs = output["text_features"]
+    # Get all the images
+    images = None
+    with open("options.json") as f:
+        options = json.load(f)
+        images = options["images"]
+    if not images:
+        images = [args.i]
     
-    # Filter out low-confidence entries
-    filter_condition = scores > args.pt
-    boxes = boxes[filter_condition]
-    scores = scores[filter_condition]
-    classes = classes[filter_condition]
-    rms = rms[filter_condition]
-    tfs = tfs[filter_condition]
-    
-    boxes[:, 0::2] /= scale_x
-    boxes[:, 1::2] /= scale_y
-    
-    texts = []
-    for tf in tfs:
-        # Perform the second inference - for text encoding
-        tf = encoder_exec.infer({"input": tf})["output"]
-        tf = np.reshape(tf, (tf.shape[0], tf.shape[1], -1))
-        tf = np.transpose(tf, (0,2,1))
+    # Now loop through all the images, and perform inference on each one of them
+    for input_image in images:
+        # Read the input image texts
+        image = cv2.imread(input_image)
+        scale_x = w / image.shape[1]
+        scale_y = h / image.shape[0]
+        image = cv2.resize(image, (w, h))
         
-        hidden = np.zeros(hidden_shape) 
-        prev_symbol_index = np.ones((1,)) * SOS_INDEX
-
-        text = ""
-        # Peform the third inference - for text decoding
-        for i in range(MAX_SEQ_LEN):
-            decoder_output = decoder_exec.infer({
-                "prev_symbol": prev_symbol_index,
-                "prev_hidden": hidden,
-                "encoder_outputs": tf
-            })
-
-            symbols_distr = decoder_output["output"]
-            prev_symbol_index = int(np.argmax(symbols_distr, axis=1))
-            if prev_symbol_index == EOS_INDEX or prev_symbol_index >= len(args.letters):
-                break
-            text += args.letters[prev_symbol_index]
-            hidden = decoder_output["hidden"]
-        texts.append(text)
+        # Apply preprocessing on the image
+        image_size = image.shape[:2]
+        image = np.pad(
+            image, (
+            (0, h - image_size[0]), 
+            (0, w - image_size[1]),
+            (0, 0)
+            ),
+        mode='constant', constant_values=0
+        )
+        image = image.transpose((2, 0, 1))
+        image = image.reshape((n, c, h, w)).astype(np.float32)
         
-
-    print(texts)
+        # Get the info for inference
+        info = np.asarray([[image_size[0], image_size[1], 1]], dtype=np.float32)
+    
+        # Perform first inference - for text spotting - and parse the output
+        output = detector_exec.infer({"im_data": image, "im_info": info})
+    
+        boxes = output["boxes"]
+        scores = output["scores"]
+        classes = output["classes"].astype(np.uint32)
+        rms = output["raw_masks"]
+        tfs = output["text_features"]
+        
+        # Filter out low-confidence entries
+        filter_condition = scores > args.pt
+        boxes = boxes[filter_condition]
+        scores = scores[filter_condition]
+        classes = classes[filter_condition]
+        rms = rms[filter_condition]
+        tfs = tfs[filter_condition]
+        
+        boxes[:, 0::2] /= scale_x
+        boxes[:, 1::2] /= scale_y
+        
+        texts = []
+        for tf in tfs:
+            # Perform the second inference - for text encoding
+            tf = encoder_exec.infer({"input": tf})["output"]
+            tf = np.reshape(tf, (tf.shape[0], tf.shape[1], -1))
+            tf = np.transpose(tf, (0,2,1))
+            
+            hidden = np.zeros(hidden_shape) 
+            prev_symbol_index = np.ones((1,)) * SOS_INDEX
+    
+            text = ""
+            # Peform the third inference - for text decoding
+            for i in range(MAX_SEQ_LEN):
+                decoder_output = decoder_exec.infer({
+                    "prev_symbol": prev_symbol_index,
+                    "prev_hidden": hidden,
+                    "encoder_outputs": tf
+                })
+    
+                symbols_distr = decoder_output["output"]
+                prev_symbol_index = int(np.argmax(symbols_distr, axis=1))
+                if prev_symbol_index == EOS_INDEX or prev_symbol_index >= len(args.letters):
+                    break
+                text += args.letters[prev_symbol_index]
+                hidden = decoder_output["hidden"]
+            texts.append(text)
+            
+    
+        # print(texts)
+        
+        # Now dump the texts onto a text file
+        with open("dump.txt", "a") as f:
+            for text in texts:
+                text = str(text) + " "
+                f.write(text)
+            f.write("\n\n")
 
 
     # Save down the resulting image
